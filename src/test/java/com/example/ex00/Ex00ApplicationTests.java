94d7bf66c50c268b6301e5e6e9d255e662c59d39
shakecoding <hogeun4656@gmail.com>
2022-10-31 11:33:44 +0900

    first spring

---
+++ src/test/java/com/example/ex00/Ex00ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.ex00;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex00ApplicationTests {
+
+    @Test
+    void contextLoads() {
+        
+    }
+
+}
